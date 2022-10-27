9904a1b3a0a410afb7cdec39e44a83701de63b3c
Ünal <unalustk@windowslive.com>
2022-10-27 15:39:43 +0300

    signup signin functions created

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,22 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react"
+import AuthContent from "../components/Auth/AuthContent"
+import { createUser } from "../util/auth"
+import LoadingOverlay from "../components/ui/LoadingOverlay"
 
 function SignupScreen() {
-  return <AuthContent />;
+    const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+    async function signUpHandler({ email, password }) {
+        setIsAuthenticating(true)
+        await createUser(email, password)
+        setIsAuthenticating(false)
+    }
+
+    if (isAuthenticating) {
+        return <LoadingOverlay message={"Creating user..."} />
+    }
+
+    return <AuthContent onAuthenticate={signUpHandler} />
 }
 
-export default SignupScreen;
+export default SignupScreen
