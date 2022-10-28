55c11c832d2337b9fabc33536ca7ab84a0c78b41
Ünal <unalustk@windowslive.com>
2022-10-28 11:24:07 +0300

    signup signin error handling

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -2,13 +2,21 @@ import { useState } from "react"
 import AuthContent from "../components/Auth/AuthContent"
 import { createUser } from "../util/auth"
 import LoadingOverlay from "../components/ui/LoadingOverlay"
+import { Alert } from "react-native"
 
 function SignupScreen() {
     const [isAuthenticating, setIsAuthenticating] = useState(false)
 
     async function signUpHandler({ email, password }) {
         setIsAuthenticating(true)
-        await createUser(email, password)
+        try {
+            await createUser(email, password)
+        } catch (error) {
+            Alert.alert(
+                "Auth failed!",
+                "Could not sign you up. Please check your credentials or try again later!"
+            )
+        }
         setIsAuthenticating(false)
     }
 
